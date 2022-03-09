43781b991c9b85868d7443b28df033808428c3e1
issei_ezawa <issemann@yahoo.co.jp>
2022-03-10 03:43:12 +0900

    試し

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -22,6 +22,9 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each { |event|
+      userId = event['source']['userId']  #userId取得
+      p 'UserID: ' + userId # UserIdを確認
+      p 'いっせい'
       case event
       when Line::Bot::Event::Message
         case event.type
