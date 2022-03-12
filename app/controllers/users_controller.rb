9ea4af02a5997fcd8ca7f94d983f1cfef4b1861a
issei_ezawa <issemann@yahoo.co.jp>
2022-03-12 19:18:04 +0900

    binding.pry 削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   def create
     money = Money.find_by(name: params[:name], yen: params[:yen])
     if money
-      binding.pry
       @user = User.find(money.user_id)
       redirect_to user_path(@user)
     else
