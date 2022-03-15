60820ce35977fe15856ef72a9bd3d18c028ce0de
issei_ezawa <issemann@yahoo.co.jp>
2022-03-16 01:52:15 +0900

    高順に変更

--- app/models/money.rb
+++ app/models/money.rb
@@ -1,6 +1,6 @@
 class Money < ApplicationRecord
   belongs_to :user
 
-  scope :year_month_day_serch, -> { group("EXTRACT(year FROM created_at), EXTRACT(month FROM created_at), EXTRACT(day FROM created_at)").sum(:yen) }
-  scope :year_month_serch, -> { group("EXTRACT(year FROM created_at), EXTRACT(month FROM created_at)").sum(:yen) }
+  scope :year_month_day_serch, -> { group("EXTRACT(year FROM created_at), EXTRACT(month FROM created_at), EXTRACT(day FROM created_at)").sum(:yen).sort.reverse.to_h }
+  scope :year_month_serch, -> { group("EXTRACT(year FROM created_at), EXTRACT(month FROM created_at)").sum(:yen).sort.reverse.to_h }
 end
